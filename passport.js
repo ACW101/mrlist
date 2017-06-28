7b61ecdc77319c535a9c8da0e87873031a4195f8
Kevin Wang <kevinwang101@gmail.com>
2017-06-28 08:05:40 +0800

    api key separated and gitigmnored

--- passport.js
+++ passport.js
@@ -4,11 +4,9 @@ const knex = require("knex");
 const passport = require("passport")
   , FacebookStrategy = require('passport-facebook').Strategy;
 
-passport.use(new FacebookStrategy({
-    clientID: "689442521266270",
-    clientSecret: "1bbd0d9343640143e80f7dd1c9f39fd0",
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
-  },
+const facebookKey = require('./facebookKey');
+
+passport.use(new FacebookStrategy(facebookKey,
   function(accessToken, refreshToken, profile, done) {
     console.log(profile);
     db("users")
