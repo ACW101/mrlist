b9abf724edcfea8eb7172cdada5c63bc7ed069c0
Kevin Wang <kevinwang101@gmail.com>
2018-01-13 15:41:36 -0500

    implement scraper to scrape all pages

--- utility/yelpBookmarkScraper.js
+++ utility/yelpBookmarkScraper.js
@@ -6,18 +6,23 @@ process.on('message', (yelp_id) => {
 })
 
 function getBookmarks(yelp_id) {
-    scraperjs.StaticScraper
-    .create(`https://www.yelp.com/user_details_bookmarks?userid=${yelp_id}`)
-    .scrape( $ => {
-        return $(".biz-name.js-analytics-click")
-            .map( (i, e) => $(e).attr('href') )
-            
-    })
-    .then( bookmarks => {
-        for (var i = 0; i < bookmarks.length; i++) {
-            process.send(bookmarks[i].substring(5))
-        }
-        process.send('eof')
-        process.exit(0)
-    })
+    let start = 0;
+    let scraper = scraperjs.StaticScraper
+        .create()
+        .scrape( $ => {
+            return $(".biz-name.js-analytics-click")
+                .map( (i, e) => $(e).attr('href')).get();
+        })
+        .then( bookmarks => {
+            if (bookmarks.length == 0) {
+                process.send('eof')
+            } else {
+                start += 50
+                for (var i = 0; i < bookmarks.length; i++) {
+                    process.send(bookmarks[i].substring(5))
+                }
+                scraper.get(`https://www.yelp.com/user_details_bookmarks?userid=${yelp_id}&start=${start}`)
+            }
+        })
+    scraper.get(`https://www.yelp.com/user_details_bookmarks?userid=${yelp_id}&start=${start}`)
 }
